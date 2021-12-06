681812e648d732fb453428ae49bc6abec30ce72e
thirtyThreeS <thaumasios@outlook.com>
2021-12-06 14:25:27 -0500

    initial commit

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿//Possible steps:
+//DONE - Create an Interface named IVehicle that has a stubbed out public void Drive method
+//DONE - Create 2 new public classes that will conform to IVehicle
+//DONE - Example) Car, Motorcycle,  BigRig
+//DONE - These classes must conform to IVehicle and implement the Drive() method - which will just Console.WriteLine(“Building a new Car!”)
+//DONE - Now we will make our static VehicleFactory class
+//DONE - It will contain a static method GetVehicle(), that will return an IVehicle based on the amount of tires it’s given as a parameter
+//Call this functionality in the Main method
+using FactoryPattern;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var getVehicle = VehicleFactory.GetVehicle();
+
+            getVehicle.Drive();
+        }
+    }
+}
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryPattern;
+
+
+    public class VehicleFactory
+    {
+        public static IVehicle GetVehicle()
+        {
+            Console.Write("How many tires do you wish your new vehicle to have? (Pick 2 or 4): ");
+           
+            var tires = Console.ReadLine();
+
+            switch (tires)
+            {
+                case "2":
+                    return new Motorcycle();
+                case "4":
+                    return new Truck();
+                default:
+                    return new Truck();
+            }
+        }
+    }
+
